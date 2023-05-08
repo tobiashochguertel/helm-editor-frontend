48c474d833698b79cb4b13f4d1955a7231cd0986
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2023-05-08 19:08:19 +0200

    Cleanup

--- src/webapp/App.tsx
+++ src/webapp/App.tsx
@@ -11,6 +11,19 @@ type FileContent = {
   content: string
 }
 
+async function getContent(filename: string): Promise<FileContent> {
+  // http://localhost:5173/api/file/templates%2Fworker-deployment.yaml
+  const SLASH = '%2F';
+  if (filename.indexOf("/") > -1) {
+    filename = filename.replace("/", SLASH);
+  }
+
+  const data = await (
+    await fetch(`/api/file/${filename}`)
+  ).json();
+  return data;
+}
+
 function App() {
   const { setToast } = useToasts()
 
@@ -23,18 +36,6 @@ function App() {
   const [filesAndContent, setFilesAndContent] = useState<Map<string, string>>(new Map<string, string>());
   const [filesContentLoaded, setFilesContentLoaded] = useState(false);
 
-  async function getContent(filename: string): Promise<FileContent> {
-    // http://localhost:5173/api/file/templates%2Fworker-deployment.yaml
-    const SLASH = '%2F';
-    if (filename.indexOf("/") > -1) {
-      filename = filename.replace("/", SLASH);
-    }
-
-    const data = await (
-      await fetch(`/api/file/${filename}`)
-    ).json();
-    return data;
-  }
   useEffect(() => {
     async function getFiles() {
       if (files.length == 0) return;
@@ -103,17 +104,6 @@ function App() {
     <>
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-        {/* <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-          data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <input className="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
-          aria-label="Search">
-          <div className="navbar-nav">
-            <div className="nav-item text-nowrap">
-              <a className="nav-link px-3" href="#">Sign out</a>
-            </div>
-          </div> */}
       </header>
 
       <Container fluid className="">
@@ -137,46 +127,20 @@ function App() {
                   <PlusCircle className='feather align-text-bottom' />
                 </a>
               </h6>
-              <ul className="nav flex-column mb-2">
-                {/* <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <span data-feather="file-text" className="align-text-bottom"></span>
-                    Current month
-                  </a>
-                </li> */}
-              </ul>
-
               <Tree
                 onClick={handler}
                 value={filesTree}
               />
-
             </div>
           </nav>
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            {/* <div
-              className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-              <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group me-2">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
-                </div>
-                <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                  <span data-feather="calendar" className="align-text-bottom"></span>
-                  This week
-                </button>
-              </div>
-            </div> */}
-
             {selectedFile && <Editor
               input={code}
               output={output}
               onChange={onChange}
               selectedFile={selectedFile}
             />}
-
           </main>
 
         </Row>
